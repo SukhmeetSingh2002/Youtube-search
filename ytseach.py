545a05efebae433d18ebb61e623fee0f40d43777
SukhmeetSingh <sukhmeet2002@gmail.com>
2021-03-23 17:34:22 +0530

    Added command line input

--- ytseach.py
+++ ytseach.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 import time
+import argparse
 links=[]
 
-# input data from user
-query=input("Input your query : ")
-number=1
-number=int(input("Enter which query to be opened : "))
+# taking input from command line
+parser=argparse.ArgumentParser()
+
+# adding arguments
+parser.add_argument('query',help='Enter the query to search on youtube')
+parser.add_argument('-n','--number',help='Query number which is to be opened',type= int,default=1)
+args=parser.parse_args()
+
+
+query=args.query
+number=args.number
 
 # opens firefox
 driver=webdriver.Firefox(executable_path="D:\\Driver\\geckodriver.exe")
@@ -13,7 +21,7 @@ url=f"https://www.youtube.com/results?search_query={query}"
 driver.get(url)
 
 try:
-    # opena the nth video requested by the user
+    # opens the nth video requested by the user
     xxpath=(driver.find_elements_by_xpath('//*[@id="video-title"]'))
     time.sleep(3)
     xxpath[number-1].click()
